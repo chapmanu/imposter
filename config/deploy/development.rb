47178475597f5aa7636a9d9f61c0dd5606e84397
mirabelrice <rice122@mail.chapman.edu>
2017-08-08 16:16:47 -0700

    fixed deploy setup for development

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -1,3 +1,11 @@
  server "localhost:5555", user: "deploy", roles: %w{app web}
 
- set :branch, ENV['BRANCH'] || 'master'
+# The git branch for staging
+def current_git_branch
+  branch = `git symbolic-ref HEAD 2> /dev/null`.strip.gsub(/^refs\/heads\//, '')
+  puts "Deploying branch #{branch}"
+  branch
+end
+
+# Set the deploy branch to the current branch
+set :branch, current_git_branch
