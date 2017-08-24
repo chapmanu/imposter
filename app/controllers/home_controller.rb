f1d71400f9405fa975340c86026f52d2912464a7
Kayla Ziegler <kziegler@chapman.edu>
2017-08-24 10:48:56 -0700

    moved rave actions into home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,28 @@
+require 'rss'
+require 'open-uri'
+
 class HomeController < ActionController::Base
   layout 'application'
 
   def index
+    @status = Rails.cache.read("rave_status")
   end
+
+  def send_alert
+    if request[:status] == "on"
+      status = "off"
+    else
+      status = "on"
+    end
+
+    Rails.cache.write("rave_status", status)
+    redirect_to :action => 'index'
+  end
+
+  def live_feed
+    url = "http://www.getrave.com/rss/chapman/channel1"
+    open(url) do |rss|
+      feed = RSS::Parser.parse(rss, do_validate=false)
+    end
+  end 
 end
---
+++ app/controllers/rave_controller.rb
@@ -0,0 +1,12 @@
+class RaveController < ActionController::Base
+  layout 'application'
+
+  def index
+    @status = Rails.cache.read("rave_status")
+
+    respond_to do |format|
+      format.rss { render :layout => false }
+    end
+  end
+
+end
